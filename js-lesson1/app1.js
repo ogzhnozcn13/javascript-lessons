fa9b3a1d4b3666b5b6bf128cee35a5016fc260ec
ogzhnozcn13 <o.ozcan.91@gmail.com>
2022-12-08 12:57:58 +0300

    My First Push

---
+++ js-lesson1/app1.js
@@ -0,0 +1,24 @@
+console.log("object");
+
+console.log("Naber nasılsın?");
+
+document.write("Hello World!");
+
+window.alert("Hi!")
+
+let age= prompt("How old are you?",100);
+console.log(age);
+alert(`You are ${age} years old`);
+
+
+window.confirm("Siteye girmek istiyor musun?")
+
+let isLegit= prompt("What is your age?")
+alert("it is legal your age.")
+console.log(isLegit);
+
+var x = 5;
+var y = 7;
+y=x;
+console.log(x);
+console.log(y);
