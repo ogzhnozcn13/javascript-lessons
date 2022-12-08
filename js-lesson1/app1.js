a80ad1b7a84e4f0ee90846a56000e74a86b608b7
ogzhnozcn13 <o.ozcan.91@gmail.com>
2022-12-08 15:54:22 +0300

    My First Push

--- js-lesson1/app1.js
+++ js-lesson1/app1.js
@@ -1,24 +1,50 @@
+// console.log("object");
+
+// console.log("Naber nasılsın?");
+
+// document.write("Hello World!");
+
+// window.alert("Hi!")
+
+// let age= prompt("How old are you?",100);
+// console.log(age);
+// alert(`You are ${age} years old`);
+
+// window.confirm("Siteye girmek istiyor musun?")
+
+// let isLegit= prompt("What is your age?")
+// alert("it is legal your age.")
+// console.log(isLegit);
+
+// var x = 5;
+// var y = 7;
+// y=x;
+// console.log(x);
+// console.log(y);
+
+function BenimKodum(){
+    var a=5
+    console.log(a);
+}
 console.log("object");
-
-console.log("Naber nasılsın?");
-
-document.write("Hello World!");
-
-window.alert("Hi!")
-
-let age= prompt("How old are you?",100);
-console.log(age);
-alert(`You are ${age} years old`);
+BenimKodum("a")
 
 
-window.confirm("Siteye girmek istiyor musun?")
+function İlkKodlarim(){
+    let x = 7;
+    if (x == 7){
+        let y = 17;
+        console.log(x + "" + y);
+    }
+}
+İlkKodlarim()
 
-let isLegit= prompt("What is your age?")
-alert("it is legal your age.")
-console.log(isLegit);
 
-var x = 5;
-var y = 7;
-y=x;
-console.log(x);
-console.log(y);
+function ders(){
+    let c = 9;
+    if (c == 9){
+        let d = 5;
+        console.log(c - "" - d);
+    }
+}
+ders()
