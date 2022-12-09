fdf7973d42e1b2433d94afa5282afd507ceba42a
ogzhnozcn13 <o.ozcan.91@gmail.com>
2022-12-09 16:51:57 +0300

    My Second Push

---
+++ js-lesson2/app2.js
@@ -0,0 +1,111 @@
+let x = 5;
+let y = 15;
+let z = x + y;
+document.write(z);
+
+
+
+let a = 5;
+let b = (50+a) * 5 + 10 -4 +9;
+document.getElementById("id1").innerHTML = b;
+
+
+
+let c = 45;
+    console.log(c%2);
+
+let e = 10345;
+    console.log(e%10);
+
+let f = a;
+    g = a += 20;
+    console.log(g);
+
+
+
+// text = "5" === "5";
+// console.log(text);
+
+// text1 = 5 === "5";
+// console.log(text1);
+
+
+
+let text1 = "What a very?";
+text1 += " Nice Day";
+console.log(text1);
+document.write(text1);
+
+
+
+// ----------------------------------------------------?????
+// text = "helen" === 3;
+// console.log(text);
+// d = "oguz"
+//     if (text=true)
+//         document.write(text1);
+//         console.log(text1);
+//     else
+//         console.log(d);
+    
+//     document.write(d);
+// ----------------------------------------------------?????
+
+
+
+
+let sayi = 25;
+    if(sayi != 25)
+        console.log("YOK");
+    else
+        console.log("VAR");
+
+
+
+function ilk_fonksiyon(){
+    let a1 = 45;
+    let b1 = 48;
+    let c1 = 45;
+    let textt1 = "yazim";
+    let textt2 = "yazim2";
+        if(a1 == 45 || b1 == 38)
+        console.log(textt1);
+        else
+        console.log(textt2);
+}
+ilk_fonksiyon()
+
+
+
+let text3 = "Hello";
+    text3 += " World";
+    console.log(text3);
+
+
+
+let q = ["at", " avrat", " silah", " mezar"];
+console.log(q[0] + q[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
