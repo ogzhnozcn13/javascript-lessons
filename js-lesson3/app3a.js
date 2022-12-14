d35ddf6bb9b28047309719ad4d88fd3e05466fe1
ogzhnozcn13 <o.ozcan.91@gmail.com>
2022-12-14 16:48:35 +0300

    14 Aralık

--- js-lesson3/app3a.js
+++ js-lesson3/app3a.js
@@ -37,3 +37,80 @@ const y = x.length-1;
 console.log(y);
 console.log(x[y]);
 console.log(x[x.length-7]);
+
+
+
+
+//? ----------------------------- Konu Başlığı: Modifiying Array Elements: ---------------------------
+
+const country = ['Albania','Bolivia','Canada','Denmark','Ethiopia','Finland','Germany','Hungary','Ireland','Japan','Kenya'];
+
+country[5] = "USA";
+console.log(country);
+
+console.log(country [country.length-5]);
+country [country.length-5] = "England";
+console.log(country);
+
+
+
+
+const xx = Array(5).fill(3);
+console.log(xx);
+
+
+
+
+const r = [1,2,3].concat([4,5,6]);
+console.log(r);
+
+
+
+//?--------------------------------------------------------------------------------------------------
+const numbers = [1,2,3,4,5];
+console.log(numbers.indexOf(5));
+console.log(numbers.indexOf(0));
+console.log(numbers.indexOf(1));
+console.log(numbers.indexOf(7));
+
+
+const w = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus iusto excepturi reprehenderit maiores necessitatibus, cum molestiae sequi veritatis in. Et cupiditate sunt sapiente. Quidem fugiat doloribus iure autem ad repudiandae?";
+const n = w.split(" ");
+console.log(n);
+console.log(n.indexOf("sunt"));
+console.log(n.lastIndexOf("fugiat"));
+
+console.log(n.includes("amet"));
+//?-----------------------------------------------------------------------------------------------------
+
+
+
+const numaralar = [1,2,3,4,5,6,7,8,"Okul yolu düz gider."];
+const strings = numaralar.toString()
+console.log(strings);
+
+
+
+const  namesx = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
+const names1 = namesx.join();
+console.log(names1);
+const names2 = namesx.join(";");
+console.log(names2);
+const names3 = namesx.join(",abc,");
+console.log(names3);
+const names4 = namesx.join("#");
+console.log(names4);
+
+
+
+
+const nums = [1,2,3,4,5,15,6,7,8,9];
+const numsChild = nums.slice(4,9);
+console.log(numsChild);
+
+
+
+const m1 = ["a", "b", "c", "d", "e", "f"];
+const n1 = m1.splice(1, 3, "v", "y", "z", "q");
+console.log(m1);
+console.log(n1);
