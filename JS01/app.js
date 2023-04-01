d73b7f91cdde733183d66ed269570d3a127f1a05
ogzhnozcn13 <o.ozcan.91@gmail.com>
2023-04-01 10:56:11 +0300

    1 April

---
+++ JS01/app.js
@@ -0,0 +1,85 @@
+const  fs = "Cohort-13";
+console.log(fs);
+
+
+const num = 20;
+console.log(num);
+
+console.log(typeof num);
+
+
+const num2 = "15";
+console.log(num2);
+
+console.log(typeof num2);
+
+// num2 = 44;
+
+const s1 = "hello "
+const s2 = "fs-13"
+console.log(s1+s2);
+
+const n1 = 5;
+const n2 = 10;
+console.log(n1+n2);
+
+const n3 = "5";
+const n4 = 10;
+console.log(n3+n4);
+
+console.log(n4+1);
+
+// n4 = n4+1;
+// console.log(n4);
+
+const numb1 = 9;
+const numb2 = 6;
+const res = numb1+numb2;
+console.log(res);
+// console.log(++res);
+
+console.log("*****************************");
+
+let lang = "js";
+console.log(lang);
+
+lang = "react";
+console.log(lang);
+
+lang = true;
+console.log(lang, typeof lang);
+
+let cohort;
+console.log(cohort, typeof cohort);
+
+// let cohort = 13;
+
+let nu1 = 5;
+let nu2 = 3;
+let result = nu1+nu2;
+console.log(result);
+console.log(++result);
+console.log(result);
+
+console.log("******************************");
+
+count = 55;
+console.log(count);
+
+var x = 13;
+console.log(x);
+
+{
+    let a = 2;
+    const b = 3;
+    console.log(a,b);
+}
+
+// console.log(a,b);
+
+{
+    var c =7;
+    console.log(c);
+}
+
+console.log(c);
